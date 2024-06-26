3eda121228882eead288a8eec7ca89a6414def93
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-06-26 18:28:46 +0600

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,j,t;
+    scanf("%d",&t);
+
+
+        for(i=1;i<=t;i++)
+        {
+            scanf("%d",&n);
+            printf("Case %d: = ",i);
+            for(j=1;j<=n;j++)
+            {
+                if(n%j==0)
+                {
+                    printf("%d ",j);
+                }
+            }
+            printf("\n");
+        }
+
+
+}
