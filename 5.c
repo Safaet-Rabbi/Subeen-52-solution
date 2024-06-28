04409cc6d392f27fe426753573685bba734340bf
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-06-28 20:37:20 +0600

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int t,n,i,j;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d",&n);
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                printf("* ");
+            }
+
+            printf("\n");
+        }
+
+    }
+}
